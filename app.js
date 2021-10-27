4fe5b26b8f19040dfb34a6eb2c0d9229cdc02365
rinkusinghh <rinkusingh05011999@gmail.com>
2021-10-27 16:38:44 +0530

    modify app.js

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ const port = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV == 'production'){
     app.use(express.static("client/build"));
-}
+    const path = require('path');
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+};
 
 app.listen(port, () => console.log(
     `server listening on the port: ${port}`)
